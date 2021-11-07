92dc01be502101fe3261ddaaf33eef44efd25518
Justine Liene Laumane <justinellaumane@gmail.com>
2021-11-07 23:08:18 +0200

    DB and Menu changes

--- app/src/main/java/com/example/bidit/AddProduct.java
+++ app/src/main/java/com/example/bidit/AddProduct.java
@@ -3,30 +3,34 @@ package com.example.bidit;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class AddProduct extends AppCompatActivity {
 
-public class HomeActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
-
+    DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
-
+        setContentView(R.layout.activity_add_product);
         drawerLayout = findViewById(R.id.my_drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -35,11 +39,11 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent;
                 switch (item.getItemId()) {
                     case R.id.nav_home:
-                        intent = new Intent(HomeActivity.this, HomeActivity.class);
+                        intent = new Intent(AddProduct.this, HomeActivity.class);
                         startActivity(intent);
                         return true;
                     case R.id.nav_add:
-                        intent = new Intent(HomeActivity.this, AddProduct.class);
+                        intent = new Intent(AddProduct.this, AddProduct.class);
                         startActivity(intent);
                         return true;
                     case R.id.nav_reserved:
@@ -56,15 +60,28 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
         actionBarDrawerToggle.syncState();
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtName = findViewById(R.id.editTxtName);
+                EditText txtDescription = findViewById(R.id.editTxtDescription);
+                EditText txtPrice = findViewById(R.id.editTxtPrice);
+                Product product = new Product(txtName.getText().toString(),txtDescription.getText().toString()); //,Double.valueOf(txtPrice.getText().toString())
+                db = FirebaseDatabase.getInstance().getReference();
+                db.child(txtName.getText().toString()).setValue(txtDescription.getText().toString());
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/example/bidit/Product.java
@@ -0,0 +1,15 @@
+package com.example.bidit;
+
+public class Product {
+    String name, description;
+    Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, String description) {
+        this.name = name;
+        this.description = description;
+        //this.price = price;
+    }
+}
